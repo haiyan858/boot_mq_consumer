fe86f18544b0fd591a110b4b1f919b8762e4054e
cuihaiyan <haiyan.cui@zebra-c.com>
2019-07-12 14:59:08 +0800

    @JmsListener 监听队列消费者

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # boot_mq_consumer
+
+ 队列消费者:
+
+```java
+@JmsListener(destination = "${myqueue}") //消息监听
+```
+
---
+++ src/main/java/com/atguigu/boot/activemq/MainApp_Consumer.java
@@ -0,0 +1,17 @@
+package com.atguigu.boot.activemq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author cuihaiyan
+ * @Create_Time 2019-07-12 14:23
+ */
+@SpringBootApplication
+public class MainApp_Consumer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainApp_Consumer.class);
+    }
+
+}
